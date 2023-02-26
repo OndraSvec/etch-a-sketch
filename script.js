bc43d71929895216cd55f3a7e0e84f29288ddbd9
Ondra Svec <ondrasvecveslo@gmail.com>
2023-02-26 08:43:06 +0100

    Add pseudocode for the app

---
+++ script.js
@@ -0,0 +1,3 @@
+//Create a num X num grid of square divs
+//Have user input the num using the provided input slider range
+//When user "hovers" over a particular grid div, change its background color
